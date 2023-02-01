9e60a297283fbeb3dfe780cb606cfab13e2a111a
GuzlejM <martin.guzlej@gmmail.com>
2023-02-01 19:13:44 +0100

    swap tokens for USD

--- FundMe.sol
+++ FundMe.sol
@@ -7,10 +7,18 @@
 pragma solidity ^0.8.8;
 
 contract FundMe { 
-  function fund() public {
+  
+  uint256 public minimumUSD = 50;
+  
+  function fund() public payable{
     // Want to be able to set a minimum fund amount in USD
     // 1. How we send ETH to this contract
+    // With payable we can access "value" attribute msg.value; in ur func
+    require(msg.value >= minimumUSD, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 
+    // how do we change msf.value into minimumUSD ?
   }
+
+
 
   function withdraw() {}
 }
