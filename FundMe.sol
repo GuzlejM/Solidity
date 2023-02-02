3a21e18d3b99147ad3dc61f19efea43bedfc099d
GuzlejM <martin.guzlej@gmmail.com>
2023-02-02 21:18:25 +0100

    minimuUsd_setup

--- FundMe.sol
+++ FundMe.sol
@@ -7,18 +7,18 @@
 pragma solidity ^0.8.8;
 
 contract FundMe { 
-  
-  uint256 public minimumUSD = 50;
-  
+
+  uint256 public minimunUsd = 50;
+
   function fund() public payable{
     // Want to be able to set a minimum fund amount in USD
     // 1. How we send ETH to this contract
     // With payable we can access "value" attribute msg.value; in ur func
-    require(msg.value >= minimumUSD, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 
-    // how do we change msf.value into minimumUSD ?
+    require(msg.value > minimunUsd, "Didn't send enough!"); // 1e18 == 1 * 10 ** 18 == 
+
+    // What is Reverting 
+    // undo any action before, and send remaining gas back
   }
 
-
-
-  function withdraw() {}
+  // function withdraw() {}
 }
